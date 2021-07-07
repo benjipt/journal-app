2f536fba6d8d622fe79d36241921fbdd317578f2
Benji Thompson <thompson.benji@gmail.com>
2021-07-07 11:51:31 -0400

    npm start opens React App successfully

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export default class App extends Component {
   render() {
     return (
-      <div>
-        
+      <div className="container text-center mt-4">
+        <h1>This is a React App</h1>
       </div>
     )
   }
