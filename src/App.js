4f3244efe996ca74a3fa56f0c0ee7d809dc989af
Benji Thompson <thompson.benji@gmail.com>
2021-07-07 18:18:34 -0400

    CreateForm component added, renders on page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
-import AddButton from './components/AddButton'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import CreateButton from './components/CreateButton';
+import CreateForm from './components/CreateForm';
 
 export default class App extends Component {
+
   render() {
     return (
       <div className="container text-center mt-4">
         <h1 className="display-1">Journals</h1>
-        <AddButton />
+        <Router>
+          <Link to="/create">
+            <CreateButton />
+          </Link>
+          <Switch>
+            <Route path="/create">
+              <CreateForm />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     )
   }
---
+++ src/components/CreateForm.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+export default class CreateForm extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            title: '',
+            body: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({ [event.currentTarget.id]: event.currentTarget.value })
+      }
+
+    render() {
+        return (
+            <div className="mt-4">
+                <form>
+                    <div className="mb-3">
+                        {/* <label htmlFor="title" className="form-label">Title</label> */}
+                        <input onChange={ this.handleChange } type="text" className="form-control" id="title" name="title" placeholder="Title goes here..." />
+                        <textarea onChange={ this.handleChange } type="text" className="form-control" id="body" name="body" placeholder="Body text goes here..." rows="6"/>
+                    </div>
+                    <div className="mb-4 d-grid gap-2">
+                        <input type="submit" className="btn btn-lg btn-success" />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
