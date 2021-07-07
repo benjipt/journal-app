827759fe1775430f5c9dfce1a322c75c5ff4cb9a
Benji Thompson <thompson.benji@gmail.com>
2021-07-07 16:38:56 -0400

    AddButton component renders on page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import AddButton from './components/AddButton'
 
 export default class App extends Component {
   render() {
     return (
       <div className="container text-center mt-4">
         <h1 className="display-1">Journals</h1>
+        <AddButton />
       </div>
     )
   }
