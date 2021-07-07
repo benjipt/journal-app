99758f838b6a8df1bd61e0ab67993ffbbcf1b894
Benji Thompson <thompson.benji@gmail.com>
2021-07-07 18:59:17 -0400

    refactored into SPA from MPA for simplicity. Clicking Add New button now hides button and shows form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import CreateButton from './components/CreateButton';
 import CreateForm from './components/CreateForm';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      showCreateForm: false
+    }
+
+    this.toggleShowButton = this.toggleShowButton.bind(this)
+  }
+
+  toggleShowButton() {
+    this.setState({ showCreateForm: !this.state.showCreateForm })
+  }
 
   render() {
     return (
       <div className="container text-center mt-4">
         <h1 className="display-1">Journals</h1>
-        <Router>
-          <Link to="/create">
-            <CreateButton />
-          </Link>
-          <Switch>
-            <Route path="/create">
-              <CreateForm />
-            </Route>
-          </Switch>
-        </Router>
+        { !this.state.showCreateForm && <CreateButton toggleShowButton={ this.toggleShowButton } /> }
+        { this.state.showCreateForm && <CreateForm /> }
       </div>
     )
   }
