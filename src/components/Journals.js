394b497fa5bce748a5988e519ded6b292bce3426
Benji Thompson <thompson.benji@gmail.com>
2021-07-08 18:13:12 -0400

    improvements to layout of cards in Journals component

--- src/components/Journals.js
+++ src/components/Journals.js
@@ -3,15 +3,17 @@ import React, { Component } from 'react'
 export default class Journals extends Component {
     render() {
         return (
-            <div className="row align-items-center">
+            <div className="container px-4">
                 { this.props.journals.map(journal => {
                     return (
-                    <div className="col-3 card">
-                        <div className="card-body">
-                            <h5 className="card-title mb-2">{ journal.title }</h5>
-                            <p className="card-text">{ journal.body }</p>
+                        <div className="row justify-content-center mb-2">
+                            <div className="col-8 card" key={journal._id}>
+                                <div className="card-body">
+                                    <h5 className="card-title mb-2">{ journal.title }</h5>
+                                    <p className="card-text">{ journal.body }</p>
+                                </div>
+                            </div>
                         </div>
-                    </div>
                     )
                 }) }
             </div>
