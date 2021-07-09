555d8650137ed7dda82d3358a1485b24726d4491
Benji Thompson <thompson.benji@gmail.com>
2021-07-08 21:51:20 -0400

    JournalPage renders a nice title

--- src/components/JournalPage.js
+++ src/components/JournalPage.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 
+
 export default class JournalPage extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="mt-5">
+                <h3>{ this.props.selectedJournal.title }</h3>
             </div>
         )
     }
