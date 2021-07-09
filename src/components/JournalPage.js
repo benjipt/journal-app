a89950a6f9a8c9f290f1e3bc946864a6254d3717
Benji Thompson <thompson.benji@gmail.com>
2021-07-09 11:14:51 -0400

    EDIT and DELETE buttons render on JournalPage component

--- src/components/JournalPage.js
+++ src/components/JournalPage.js
@@ -9,6 +9,8 @@ export default class JournalPage extends Component {
                 <div className="mt-4 row justify-content-center">
                     <p className="col-6">{ this.props.selectedJournal.body }</p>
                 </div>
+                <button className="btn btn-secondary me-2">EDIT</button>
+                <button className="btn btn-danger">DELETE</button>
             </div>
         )
     }
