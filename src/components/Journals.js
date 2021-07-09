7c52a71803399fb8f0fa757344e80cf4f4b7c351
Benji Thompson <thompson.benji@gmail.com>
2021-07-08 20:45:08 -0400

    button effect added on journal cards

--- src/components/Journals.js
+++ src/components/Journals.js
@@ -7,7 +7,7 @@ export default class Journals extends Component {
                 { this.props.journals.map(journal => {
                     return (
                         <div className="row justify-content-center mb-2" key={ journal._id }>
-                            <div className="col-9 card">
+                            <div className="col-9 card btn btn-custom">
                                 <div className="card-body">
                                     <h5 className="card-title mb-2">{ journal.title }</h5>
                                     <p className="card-text">{ journal.body }</p>
