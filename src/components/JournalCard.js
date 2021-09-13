ad25d2fa5042338eaeee61ec9064bc91a765803d
Benji Thompson <thompson.benji@gmail.com>
2021-09-13 17:24:45 -0400

    refactored JournalCard into functional component

--- src/components/JournalCard.js
+++ src/components/JournalCard.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class JournalCard extends Component {
-    render() {
-        return (
-            <div className="row justify-content-center mb-2">
-                <div 
-                    className="col-9 card btn btn-custom" 
-                    id={ this.props.journal._id }
-                    onClick={ this.props.toggleShowJournalPage }>
-                    <div className="card-body">
-                        <h5 className="card-title mb-2">{ this.props.journal.title }</h5>
-                        <p className="card-text">{ this.props.journal.body }</p>
-                    </div>
+export default function JournalCard(props) {
+
+    const { journal, toggleShowJournalPage } = props
+
+    return (
+        <div className="row justify-content-center mb-2">
+            <div 
+                className="col-9 card btn btn-custom" 
+                id={ journal._id }
+                onClick={ toggleShowJournalPage }>
+                <div className="card-body">
+                    <h5 className="card-title mb-2">{ journal.title }</h5>
+                    <p className="card-text">{ journal.body }</p>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
