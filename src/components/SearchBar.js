7a6a215c51ee161a4fe065d6430a17a4f142e34e
Benji Thompson <thompson.benji@gmail.com>
2021-09-13 17:49:27 -0400

    refactored SearchBar into functional component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,22 +1,24 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { ReactSearchAutocomplete } from 'react-search-autocomplete'
 
+// 3rd party component renders a warning in console when returns autocomplete results: "Encountered two children with the same key", need to investigate further or find alternative component
 
-export default class SearchBar extends Component {
-    render() {
-        return (
-            <div className="mt-2 mb-4 row justify-content-center">
-                <div className="col-6">
-                    <ReactSearchAutocomplete 
-                        items={ this.props.items }
-                        fuseOptions={{ keys: ['title', 'body'] }}
-                        resultStringKeyName='title'
-                        styling={{
-                            zIndex: 1
-                        }} 
-                        onSelect={ this.props.onSelect } />
-                </div>
+export default function SearchBar(props) {
+
+    const { items, onSelect } = props
+
+    return (
+        <div className="mt-2 mb-4 row justify-content-center">
+            <div className="col-6">
+                <ReactSearchAutocomplete 
+                    items={ items }
+                    fuseOptions={{ keys: ['title', 'body'] }}
+                    resultStringKeyName='title'
+                    styling={{
+                        zIndex: 1
+                    }} 
+                    onSelect={ onSelect } />
             </div>
-        )
-    }
+        </div>
+    )
 }
