33158dd482771eda6d272640c60076be6000865f
Benji Thompson <thompson.benji@gmail.com>
2021-09-13 17:44:31 -0400

    refactored Logout into functional component

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,30 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { GoogleLogout } from 'react-google-login'
 
 require('dotenv').config()
 // Need to troubleshoot why process.env.REACT_APP_CLIENTID is undefined
 const clientId = process.env.REACT_APP_CLIENTID || '890910246306-5u2nbfcpob0o0jk6j5tr53aanpro9pau.apps.googleusercontent.com'
 
-export default class Logout extends Component {
-    constructor(props) {
-        super(props)
+export default function Logout(props) {
 
-        this.onSuccess = this.onSuccess.bind(this)
+    const { handleLogout } = props
+
+    const onSuccess = () => {
+        handleLogout()
     }
 
-    onSuccess() {
-        this.props.handleLogout()
-    }
-
-    render() {
-        return (
-            <div className="mt-4 mb-2">
-                <GoogleLogout 
-                    clientId={clientId}
-                    buttonText="Logout"
-                    onLogoutSuccess={this.onSuccess}
-                />
-            </div>
-        )
-    }
+    return (
+        <div className="mt-4 mb-2">
+            <GoogleLogout 
+                clientId={ clientId }
+                buttonText="Logout"
+                onLogoutSuccess={ onSuccess }
+            />
+        </div>
+    )
 }
