1f0dd986da996d981c69e34e44e189061156b174
Benji Thompson <thompson.benji@gmail.com>
2021-09-17 16:46:29 -0400

    removed .env variable

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -2,8 +2,7 @@ import React from 'react'
 import { GoogleLogout } from 'react-google-login'
 
 require('dotenv').config()
-// Need to troubleshoot why process.env.REACT_APP_CLIENTID is undefined
-const clientId = process.env.REACT_APP_CLIENTID || '890910246306-5u2nbfcpob0o0jk6j5tr53aanpro9pau.apps.googleusercontent.com'
+const clientId = process.env.REACT_APP_CLIENTID
 
 export default function Logout(props) {
 
